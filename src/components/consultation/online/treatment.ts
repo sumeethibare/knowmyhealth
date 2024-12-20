d20069fad49b4e53bfc7ced5763b90b4bd7f7dae
Sumeet Hibare <122166739+sumeethibare@users.noreply.github.com>
2024-12-20 11:54:14 +0530

    Update treatment.ts

--- src/components/consultation/online/treatment.ts
+++ src/components/consultation/online/treatment.ts
@@ -5,11 +5,11 @@ import Skin from '/public/images/desease/skin.png'
 import Cancer from '/public/images/desease/cancer.png'
 
 export const diseases = [
-    { id: 1, name: "Diabetes Specialist", description: "Manage your blood sugar levels.", image: Diabetes },
+{ id: 1, name: "Radiology Specialist", description: "Manage your scans.", image: Diabetes },
+    { id: 6, name: "Diabetes Specialist", description: "Manage your blood sugar levels.", image: Diabetes },
     { id: 2, name: "Heart Treatment", description: "Heart-related consultation.", image: Heart },
     { id: 3, name: "Asthma Specialist", description: "Breathing problems and asthma management.", image: Asthma },
     { id: 4, name: "Skin Issues Specialist", description: "Treat skin-related problems.",  image: Skin },
     { id: 5, name: "Cancer Specialist", description: "Cancer treatment and consultation.", image: Cancer },
-{ id: 5, name: "Radiology Specialist", description: "Manage your scans.", image: Diabetes },
   ];
   
